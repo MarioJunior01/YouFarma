d4056ba60ae4617f0f5d55293a1feb4491e04734
mario-juniorifpr <136728961+mario-juniorifpr@users.noreply.github.com>
2023-10-10 14:27:46 -0300

    Add files via upload

---
+++ YouFarma-main (3)/YouFarma-main/src/YouFarma-main/src/Farmacia.java
@@ -0,0 +1,51 @@
+import javax.swing.JOptionPane;
+
+public class Farmacia {
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(
+        null,
+        "BEM VINDO(A) a YourFarma," +
+            "\nOnde você encontra todas as variedades e melhores marcas de produtos." +
+            "\nSomos abertos 24h, com serviço delivery em toda a Curitiba e região" +
+            "\n - Para prosseguir selecione OK.");
+
+    boolean ver = true;
+
+    while (ver) {
+      ver = selecionarAba();
+      Carrinho.exibirCarrinho();
+    }
+
+  }
+
+  // seleção das abas
+  static boolean selecionarAba() {
+    String[] abas = { "Medicamentos", "Dermocosmeticos", "Beleza", "Sair" };
+    String resposta = (String) JOptionPane.showInputDialog(
+        null,
+        "Produtos ",
+        "o que você precisa?",
+        JOptionPane.PLAIN_MESSAGE,
+        null,
+        abas,
+        abas[0]);
+
+    switch (resposta) {
+
+      case "Medicamentos":
+        Produtos.adicionarMedicamentos();
+        return Produtos.selecionarProduto();
+
+      case "Dermocosmeticos":
+        Produtos.adicionarDermocosmeticos();
+        return Produtos.selecionarProduto();
+
+      case "Beleza":
+        Produtos.adicionarSaudeBeleza();
+        return Produtos.selecionarProduto();
+
+    }
+
+    return true;
+  }
+}
